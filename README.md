bbc96ed27efa7b708ad3ac4202e001ab32684f21
Travis Benfield <benfieldta@gmail.com>
2017-11-25 13:40:24 -0500

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# friend_finder
+Friend finder application that utilizes your answers to questions to match you up most closely with others.
---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [{
+	"name": "Steven",
+	"photo": "https://mugs11.files.wordpress.com/2013/04/lookingnew.jpg",
+	"scores": [4, 4, 1, 5, 2, 4, 5, 3, 2, 2]
+}, {
+	"name": "Frank",
+	"photo": "http://nadineriopel.com/wp-content/uploads/2012/09/Mack-photo.jpg",
+	"scores": [1, 2, 4, 4, 5, 4, 2, 3, 3, 4]
+}, {
+	"name": "Linda",
+	"photo": "http://ww2.hdnux.com/photos/42/37/54/9042693/5/920x920.jpg",
+	"scores": [3, 2, 4, 3, 2, 4, 2, 3, 5, 4]
+}];
+
+
+module.exports = friends;
